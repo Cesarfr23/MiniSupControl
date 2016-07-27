8a140d46548e4d6a10ee99abf6c91cb5267a6376
Cesarfr23 <cesarfernandez860@gmail.com>
2016-07-27 16:58:01 +0100

    Agregando Nombres de las Tablas en la clase de conexion

--- MiniSupControl/DAL/MiniSubControlDb.cs
+++ MiniSupControl/DAL/MiniSubControlDb.cs
@@ -5,6 +5,7 @@ using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using Entidades;
 
 namespace DAL
 {
@@ -13,11 +14,17 @@ namespace DAL
         public MiniSubControlDb() : base("name=MiniSubControl")
         {
         }
-
+        public virtual DbSet<Categoria> Categoria { get; set; }
+        public virtual DbSet<Cliente> Cliente { get; set; }
+        public virtual DbSet<Detalle_Ingreso> Detalle_Ingreso { get; set; }
+        public virtual DbSet<Detalle_Venta> Detalle_Venta { get; set; }
+        public virtual DbSet<Ingreso> Ingreso { get; set; }
+        public virtual DbSet<Presentacion> Presentacion { get; set; }
+        public virtual DbSet<Producto> Producto { get; set; }
+        public virtual DbSet<Proveedor> Proveedor { get; set; }
+        public virtual DbSet<TipoUsuario> TipoUsuario { get; set; }
         public virtual DbSet<Usuario> Usuario { set; get; }
+        public virtual DbSet<venta> venta { get; set; }
 
-    public class DbContext
-    {
-    }
 }
 }
